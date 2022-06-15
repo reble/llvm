47251a2c442c1ef55cfc1399a74e868583830ce7
u154255 <u154255@s001-n162.aidevcloud>
2022-06-15 08:15:15 -0700

    added makefile and run.sh

---
+++ sycl/examples/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#source /opt/intel/inteloneapi/setvars.sh > /dev/null 2>&1
+
+export DPCPP_HOME=/home/u154255/sycl_graph/chchiu
+export LLVMBUILDDIR=$DPCPP_HOME/llvm/build 
+
+export PATH=$LLVMBUILDDIR/bin:$PATH
+export LD_LIBRARY_PATH=$LLVMBUILDDIR/lib:$LD_LIBRARY_PATH
+
+#fullname=$1
+#filename="${fullname%%.*}"
+#
+#clang++ -g $1 -o $filename -I$DPCPP_HOME/sycl/include -I$DPCPP_HOME/include -std=c++17 -fsycl -fsycl-unnamed-lambda 
+#export LD_LIBRARY_PATH=$LLVMBUILDDIR/lib:$LD_LIBRARY_PATH
+#./$filename 1 1
+#
+
+# clean all the executable and output texts
+make clean
+
+# compile *-sycl-usm-graph source code
+make sycl-usm-graph
+
+# execute *-sycl-usm-graph executables
+echo ""
+echo "------ Start  running executables ------"
+echo ""
+make run-sycl-usm-graph
+echo ""
+echo "------ Finish running executables ------"
+echo ""
+
+
+
+
+#export LD_LIBRARY_PATH=$LLVMBUILDDIR/lib:$LD_LIBRARY_PATH
+#rm -rf ./a.out
+#PATH=$LLVMBUILDDIR/bin:$PATH clang++ -g $1 -I $LLVMBUILDDIR/include/sycl -I $LLVMBUILDDIR/include -std=c++17 -fsycl -fsycl-unnamed-lambda 
+
+
