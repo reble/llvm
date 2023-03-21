cb2ad2bb6a8bc133c13aba296e3610233e5d7adc
Ewan Crawford <ewan@codeplay.com>
2023-03-21 09:38:44 +0000

    Update test to use asserts
    Make test consistent with other tests by using asserts
    rather than printing to std out.

--- sycl/test/graph/graph-record-temp-scope.cpp
+++ sycl/test/graph/graph-record-temp-scope.cpp
@@ -1,6 +1,5 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 
-#include <iostream>
 #include <sycl/sycl.hpp>
 
 #include <sycl/ext/oneapi/experimental/graph.hpp>
@@ -39,24 +38,12 @@ int main() {
 
   q.submit([&](sycl::handler &h) { h.ext_oneapi_graph(exec_graph); });
 
-  int errors = 0;
   // Verify results
   for (size_t i = 0; i < n; i++) {
-    if (arr[i] != expectedValue) {
-      std::cout << "Test failed: Unexpected result at index: " << i
-                << ", expected: " << expectedValue << " actual: " << arr[i]
-                << "\n";
-      errors++;
-    }
+    assert(arr[i] == expectedValue);
   }
-
-  if (errors == 0) {
-    std::cout << "Test passed successfuly.\n";
-  }
-
-  std::cout << "done.\n";
 
   sycl::free(arr, q.get_context());
 
-  return errors;
+  return 0;
 }
