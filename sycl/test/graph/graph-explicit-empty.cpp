dd322e7b2ef980ebe0c318575d65ca6f100c1e68
Pablo Reble <pablo.reble@intel.com>
2023-03-28 14:22:37 -0700

    Adding test for empty node

---
+++ sycl/test/graph/graph-explicit-empty.cpp
@@ -0,0 +1,45 @@
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+#include <iostream>
+#include <sycl/sycl.hpp>
+
+#include <sycl/ext/oneapi/experimental/graph.hpp>
+
+int main() {
+
+  sycl::property_list properties{
+      sycl::property::queue::in_order{},
+      sycl::ext::oneapi::property::queue::lazy_execution{}};
+
+  sycl::queue q{sycl::gpu_selector_v, properties};
+
+  sycl::ext::oneapi::experimental::command_graph g;
+
+  const size_t n = 10;
+  float *arr = sycl::malloc_device<float>(n, q);
+  
+  auto start = g.add();
+
+  auto init = g.add([&](sycl::handler &h) {
+      h.parallel_for(sycl::range<1>{n}, [=](sycl::id<1> idx) {
+        size_t i = idx;
+        arr[i] = 0;
+      });
+  }, {start});
+    
+  auto empty = g.add({init});
+
+  g.add([&](sycl::handler &h) {
+    h.parallel_for(sycl::range<1>{n}, [=](sycl::id<1> idx) {
+      size_t i = idx;
+      arr[i] = 1;
+    });
+  }, {empty});
+
+  auto executable_graph = g.finalize(q.get_context());
+
+  q.submit([&](sycl::handler &h) { h.ext_oneapi_graph(executable_graph); }).wait();
+
+  sycl::free(arr, q);
+
+  return 0;
+}
