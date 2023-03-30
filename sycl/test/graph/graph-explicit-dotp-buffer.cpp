c2e3b951146693cafdcb5c5ae8eb88f9190e566b
Ben Tracy <ben.tracy@codeplay.com>
2023-03-30 17:46:38 +0100

    [SYCL] Address MR comments

--- sycl/test/graph/graph-explicit-dotp-buffer.cpp
+++ sycl/test/graph/graph-explicit-dotp-buffer.cpp
@@ -24,9 +24,7 @@ int main() {
   float beta = 2.0f;
   float gamma = 3.0f;
 
-  sycl::property_list properties{sycl::property::queue::in_order{}};
-
-  sycl::queue q{sycl::gpu_selector_v, properties};
+  sycl::queue q{sycl::gpu_selector_v};
 
   sycl::ext::oneapi::experimental::command_graph g;
 
--- sycl/test/graph/graph-explicit-node-ordering.cpp
+++ sycl/test/graph/graph-explicit-node-ordering.cpp
@@ -5,9 +5,7 @@
 
 int main() {
 
-  sycl::property_list properties{sycl::property::queue::in_order{}};
-
-  sycl::queue q{sycl::gpu_selector_v, properties};
+  sycl::queue q{sycl::gpu_selector_v};
 
   sycl::ext::oneapi::experimental::command_graph g;
 
