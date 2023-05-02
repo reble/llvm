01c067225d91873472ff9fae4ef1be5c3d9b7a77
Ewan Crawford <ewan@codeplay.com>
2023-05-02 13:07:48 +0100

    [SYCL][Graph] Use doxygen style comments in graphs new header files.
    As well as the doxygen comment there are some additional
    changes I found along the way:
    * Pass queue by reference to `begin_recording()` & `end_recording()`
    * Pass node by reference to `make_edge()`
    * Add stubbed out `update()` entry-point
    * Remove unused `MParent` variable

--- sycl/include/sycl/ext/oneapi/experimental/graph.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/graph.hpp
@@ -32,18 +32,20 @@ class exec_graph_impl;
 
 } // namespace detail
 
+/// State to template the command_graph class on
 enum class graph_state {
-  modifiable,
-  executable,
+  modifiable, ///< In modifiable state, commands can be added to graph
+  executable, ///< In executable state, the graph is ready to execute
 };
 
-// Forward declaration
 class node;
 
 namespace property {
 namespace graph {
 
-// TODO: Cycle check not yet implemented.
+/// Property passed to command_graph constructor to disable checking for cycles
+///
+/// \todo Cycle check not yet implemented.
 class no_cycle_check : public ::sycl::detail::DataLessProperty<
                            ::sycl::detail::GraphNoCycleCheck> {
 public:
@@ -54,10 +56,13 @@ public:
 
 namespace node {
 
+/// Property used to define dependent nodes when creating a new node with
+/// command_graph::add()
 class depends_on : public ::sycl::detail::PropertyWithData<
                        ::sycl::detail::GraphNodeDependencies> {
 public:
   template <typename... NodeTN> depends_on(NodeTN... nodes) : MDeps{nodes...} {}
+
   const std::vector<::sycl::ext::oneapi::experimental::node> &
   get_dependencies() const {
     return MDeps;
@@ -70,6 +75,7 @@ private:
 } // namespace node
 } // namespace property
 
+/// Class representing a node in the graph, returned by command_graph::add()
 class __SYCL_EXPORT node {
 private:
   node(const std::shared_ptr<detail::node_impl> &Impl) : impl(Impl) {}
@@ -84,13 +90,20 @@ private:
   std::shared_ptr<detail::graph_impl> MGraph;
 };
 
+/// Class representing a graph in the modifiable state
 template <graph_state State = graph_state::modifiable>
 class __SYCL_EXPORT command_graph {
 public:
-  command_graph(const context &syclContext, const device &syclDevice,
-                const property_list &propList = {});
+  /// Constructor
+  /// @param SyclContext context to use for graph
+  /// @param SyclDevice device all nodes will be associated with
+  /// @param PropList optional list of properties to pass
+  command_graph(const context &SyclContext, const device &SyclDevice,
+                const property_list &PropList = {});
 
-  // Adding empty node with [0..n] predecessors:
+  /// Add an empty node to the graph
+  /// @param PropList property list used to pass [0..n] predecessor nodes
+  /// @return constructed empty node which has been added to the graph
   node add(const property_list &PropList = {}) {
     if (PropList.has_property<property::node::depends_on>()) {
       auto Deps = PropList.get_property<property::node::depends_on>();
@@ -99,7 +112,10 @@ public:
     return add_impl({});
   }
 
-  // Adding device node:
+  /// Add a command-group node to the graph
+  /// @param CGF command group function to create node with
+  /// @param PropList property list used to pass [0..n] predecessor nodes
+  /// @return constructed node which has been added to the graph
   template <typename T> node add(T CGF, const property_list &PropList = {}) {
     if (PropList.has_property<property::node::depends_on>()) {
       auto Deps = PropList.get_property<property::node::depends_on>();
@@ -108,27 +124,32 @@ public:
     return add_impl(CGF, {});
   }
 
-  // Adding dependency between two nodes.
-  void make_edge(node sender, node receiver);
+  /// Add a dependency between two nodes.
+  /// @param Src Node which will be a dependency of dest
+  /// @param Dest Node which will be dependent on src
+  void make_edge(node &Src, node &Dest);
 
+  /// Finalize modifiable graph into an executable graph
+  /// @param PropList property list used to pass properties for finalization
+  /// @return Executable graph object
   command_graph<graph_state::executable>
-  finalize(const property_list &propList = {}) const;
+  finalize(const property_list &PropList = {}) const;
 
   /// Change the state of a queue to be recording and associate this graph with
   /// it.
-  /// @param recordingQueue The queue to change state on and associate this
+  /// @param RecordingQueue The queue to change state on and associate this
   /// graph with.
   /// @return True if the queue had its state changed from executing to
   /// recording.
-  bool begin_recording(queue recordingQueue);
+  bool begin_recording(queue &RecordingQueue);
 
   /// Change the state of multiple queues to be recording and associate this
   /// graph with each of them.
-  /// @param recordingQueues The queues to change state on and associate this
+  /// @param RecordingQueues The queues to change state on and associate this
   /// graph with.
   /// @return True if any queue had its state changed from executing to
   /// recording.
-  bool begin_recording(const std::vector<queue> &recordingQueues);
+  bool begin_recording(const std::vector<queue> &RecordingQueues);
 
   /// Set all queues currently recording to this graph to the executing state.
   /// @return True if any queue had its state changed from recording to
@@ -136,26 +157,34 @@ public:
   bool end_recording();
 
   /// Set a queues currently recording to this graph to the executing state.
-  /// @param recordingQueue The queue to change state on.
+  /// @param RecordingQueue The queue to change state on.
   /// @return True if the queue had its state changed from recording to
   /// executing.
-  bool end_recording(queue recordingQueue);
+  bool end_recording(queue &RecordingQueue);
 
   /// Set multiple queues currently recording to this graph to the executing
   /// state.
-  /// @param recordingQueue The queues to change state on.
+  /// @param RecordingQueue The queues to change state on.
   /// @return True if any queue had its state changed from recording to
   /// executing.
-  bool end_recording(const std::vector<queue> &recordingQueues);
+  bool end_recording(const std::vector<queue> &RecordingQueues);
 
 private:
+  /// Constructor used internally by the runtime
+  /// @param Impl Detail implementation class to construct object with
   command_graph(const std::shared_ptr<detail::graph_impl> &Impl) : impl(Impl) {}
 
-  // Template-less implementation of add()
-  node add_impl(std::function<void(handler &)> cgf,
-                const std::vector<node> &dep);
+  /// Template-less implementation of add() for CGF nodes
+  /// @param CGF command-group function to add
+  /// @param Dep list of predecessor nodes
+  /// @return Node added to the graph
+  node add_impl(std::function<void(handler &)> CGF,
+                const std::vector<node> &Dep);
 
-  node add_impl(const std::vector<node> &dep);
+  /// Template-less implementation of add() for empty nodes
+  /// @param Dep list of predecessor nodes
+  /// @return Node added to the graph
+  node add_impl(const std::vector<node> &Dep);
 
   template <class Obj>
   friend decltype(Obj::impl)
@@ -168,17 +197,25 @@ private:
 
 template <> class __SYCL_EXPORT command_graph<graph_state::executable> {
 public:
+  /// An executable command-graph is not user constructable
   command_graph() = delete;
 
+  /// Constructor used by internal runtime
+  /// @param Graph Detail implementation class to construct with
+  /// @param Ctx Context to use for graph
   command_graph(const std::shared_ptr<detail::graph_impl> &Graph,
                 const sycl::context &Ctx);
+
+  /// Update the inputs & output of the graph
+  /// @param Graph Graph to use the inputs and outputs of
+  void update(const command_graph<graph_state::modifiable> &Graph);
 
 private:
   template <class Obj>
   friend decltype(Obj::impl)
   sycl::detail::getSyclObjImpl(const Obj &SyclObject);
 
-  // Creates a backend representation of the graph in impl
+  /// Creates a backend representation of the graph in impl
   void finalize_impl();
 
   int MTag;
